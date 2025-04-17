2bbfb7d3aacd45dd123ec3cbba0d32ecf6ec9f89
dattanironak <radattani0608@gmail.com>
2025-04-17 15:07:44 +0000

    Handle newline delimiters

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -21,4 +21,8 @@ RSpec.describe StringCalculator do
     expect(calc.add("1,2,3,4")).to eq(10)
   end
 
+  it "returns sum for numbers with new lines and commas" do
+    calc = StringCalculator.new
+    expect(calc.add("1\n2,3")).to eq(6)
+  end
 end
